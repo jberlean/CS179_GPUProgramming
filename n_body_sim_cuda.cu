de6dc25c763a0ae7521e4ce7d71ec6802b8e61d0
Aman <think@ubuntu.ubuntu-domain>
2015-05-31 01:40:18 -0700

    Debugging basic simulator

--- n_body_sim_cuda.cu
+++ n_body_sim_cuda.cu
@@ -1,7 +1,7 @@
 #include <curand.h>
 
 // macro for error-handling
-#define gpuErrchk(ans) { gpuAssert((ans), (char*)__FILE__, __LINE__); }
+#define gpuErrChk(ans) { gpuAssert((ans), (char*)__FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, char* file, int line, bool abort=true)
 {
   if (code != cudaSuccess) 
@@ -22,6 +22,24 @@ int num_threads_per_block;
 float2* particle_vels[2]; // x and y represent velocity in 2D
 float3* particle_data[2]; // x and y represent position in 2D, z represents mass
 
+__global__
+void cudaInitKernel(float2 * vels_buffer, float3 * data_buffer1, float3 * data_buffer2, float * random, float box_width, 
+                    float box_height, float min_vel, float max_vel, int num_particles)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  while (i < num_particles)
+  {
+    vels_buffer[i].x = min_vel + random[4 * i] * (max_vel - min_vel);
+    vels_buffer[i].y = min_vel + random[4 * i + 1] * (max_vel - min_vel);
+    data_buffer1[i].x = random[4 * i + 2] * box_width;
+    data_buffer1[i].y = random[4 * i + 3] * box_height;
+    data_buffer1[i].z = 1;
+
+    data_buffer2[i].z = 1;    
+
+    i += blockDim.x * gridDim.x;
+  }
+}
 
 void init_data(int h_num_particles, float box_width, float box_height, float min_vel, 
                float max_vel, int h_num_blocks, int h_num_threads_per_block) 
@@ -39,47 +57,30 @@ void init_data(int h_num_particles, float box_width, float box_height, float min
    
   // set initial values for particle_vels, particle_data on GPU
   float * random;
-  gpuErrChk(cudaMalloc((void **) &random, sizeof(float) * num_particles * 4);   
+  gpuErrChk(cudaMalloc((void **) &random, sizeof(float) * num_particles * 4));   
   
   curandGenerator_t gen;
   curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
   curandGenerateUniform(gen, random, num_particles * 4);
 
-  cudaInitKernel<<<num_blocks, num_threads_per_block>>>(random, box_width, box_height, min_vel, max_vel);
+  cudaInitKernel<<<num_blocks, num_threads_per_block>>>(particle_vels[0], particle_data[0], particle_data[1], 
+                                                        random, box_width, box_height, min_vel, max_vel, num_particles);
 
   curandDestroyGenerator(gen);
   gpuErrChk(cudaFree(random));
-}
-
-__global__
-void cudaInitKernel(float * random, float box_width, float box_height, float min_vel, float max_vel)
-{
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  while (i < num_particles)
-  {
-    particle_vels[0][i].x = min_vel + random[4 * i] * (max_vel - min_vel);
-    particle_vels[0][i].y = min_vel + random[4 * i + 1] * (max_vel - min_vel);
-    particle_data[0][i].x = random[4 * i + 2] * box_width;
-    particle_data[0][i].y = random[4 * i + 3] * box_height;
-    particle_data[0][i].z = 1;
-
-    particle_data[1][i].z = 1;    
-
-    i += blockDim.x * gridDim.x;
-  }
 }
 
 void delete_data() {
   // free all memory on GPU
   for (int i = 0; i < 2; i++)
   {
-    gpuErrChl(cudaFree(particle_vels[i]));
-    gpuErrChl(cudaFree(particle_data[i]));
+    gpuErrChk(cudaFree(particle_vels[i]));
+    gpuErrChk(cudaFree(particle_data[i]));
   }
 }
 
 __device__
-float2 get_force(int pos) {
+float2 get_force(int pos, float3 * data_old, int num_particles) {
   // sum force from every other particle
   // based on mass, position of both particles
   float2 force;
@@ -88,33 +89,33 @@ float2 get_force(int pos) {
 
   for (int i = 0; i < num_particles; i++)
   {
-    float dist_squared = pow((particle_data[pingpong][pos].x - particle_data[pingpong][i].x), 2) 
-                         + pow((particle_data[pingpong][pos].y - particle_data[pingpong][i].y), 2);  
+    float dist_squared = pow((data_old[pos].x - data_old[i].x), 2) 
+                         + pow((data_old[pos].y - data_old[i].y), 2);  
 
     if (dist_squared > 0)
     {
-      float force_magnitude = particle_data[pingpong][pos].z * particle_data[pingppong][i].z / dist_squared;
-      force.x += (particle_data[pingpong][i].x - particle_data[pingpong][pos].x) * force_magnitude / sqrt(dist_squared);
-      force.y += (particle_data[pingpong][i].y - particle_data[pingpong][pos].y) * force_magnitude / sqrt(dist_squared);
+      float force_magnitude = data_old[pos].z * data_old[i].z / dist_squared;
+      force.x += (data_old[i].x - data_old[pos].x) * force_magnitude / sqrt(dist_squared);
+      force.y += (data_old[i].y - data_old[pos].y) * force_magnitude / sqrt(dist_squared);
     }
   }
   return force;  
 }
 
 __global__
-void interact_kernel(float dt) {
+void interact_kernel(float2 * vels_old, float2 * vels_new, float3 * data_old, float3 * data_new, float dt, int num_particles) {
   // each thread handles a particle
   int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-  while (i < num_bodies)
+  while (i < num_particles)
   {
-    float2 force = get_force(i);
+    float2 force = get_force(i, data_old, num_particles);
     
-    particle_vels[1 - pingpong][i].x += force.x * dt / particle_data[0][i].z;
-    particle_vels[1 - pingpong][i].y += force.y * dt / particle_data[0][i].z;
+    vels_new[i].x += force.x * dt / data_old[i].z;
+    vels_new[i].y += force.y * dt / data_old[i].z;
     
-    particle_data[1 - pingpong][i].x += particle_vels[1 - pingpong][i].x * dt; 
-    particle_data[1 - pingpong][i].y += particle_vels[1 - pingpong][i].y * dt;
+    vels_new[i].x += vels_new[i].x * dt; 
+    vels_new[i].y += vels_new[i].y * dt;
 
     i += blockDim.x * gridDim.x;
   }
@@ -122,7 +123,9 @@ void interact_kernel(float dt) {
 
 void call_interact_kernel(float dt) {
   // call kernel
-  interact_kernel<<<num_blocks, num_threads_per_block>>>(dt);
+  interact_kernel<<<num_blocks, num_threads_per_block>>>(particle_vels[pingpong], particle_vels[1 - pingpong], 
+                                                         particle_data[pingpong], particle_data[1 - pingpong], 
+                                                         dt, num_particles);
   // update pingpong
   pingpong = 1 - pingpong;
 }
